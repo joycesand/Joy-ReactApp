788d2c84b6adc03a1541949d3c61b3ff60c9993b
sandiejoy <ajoycesandra@gmail.com>
2020-11-06 21:38:27 +0300

    done netlify folder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,7 @@ function Footer() {
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>About Us</h2>
-            <Link to='/'>How Sandiejoy works</Link>
+            <Link to='/'>How Sandiejoy Travel works</Link>
             <Link to='/'>Olypics</Link>
             <Link to='/'>Careers</Link>
             <Link to='/'>Investors</Link>
