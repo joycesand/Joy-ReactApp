07c24509d658af966b26f9ed396a9d12fa2b565c
sandiejoy <ajoycesandra@gmail.com>
2020-11-06 20:49:59 +0300

    done netlify folder

---
+++ build/precache-manifest.8f98c8fc27028375688ea80093d3959f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7257ca8334785d18827f4758a097ebbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73979b5265bdf51c3e9c",
+    "url": "/static/css/main.53f26a37.chunk.css"
+  },
+  {
+    "revision": "68c677235e173152cc54",
+    "url": "/static/js/2.7a0460fb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7a0460fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73979b5265bdf51c3e9c",
+    "url": "/static/js/main.f74b6ce1.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  }
+]);
